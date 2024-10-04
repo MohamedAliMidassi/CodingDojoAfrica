f0c72013d997fe0b4f031a3fea19558809b0f38e
MohamedAliMidassi <mohamedalimidassi@gmail.com>
2024-10-04 15:32:32 +0100

    github fixed

--- Java/Week 1/Day 2/Practice/Coffeedore64/TestOrders.java
+++
@@ -1,9 +0,0 @@
-public class TestOrders {
-    public static void main(String[] args) {
-        CoffeeKiosk menu = new CoffeeKiosk();
-        menu.addMenuItem("Mocha", 2.00);
-        menu.addMenuItem("Espresso", 1.50);
-
-        menu.newOrder();
-    }
-}
